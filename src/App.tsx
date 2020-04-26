48874e8ff06ce7a08250a59180d0615cd9a4c263
shengqiwu <shengqiwu@guanmai.cn>
2020-04-27 00:20:34 +0800

    menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import Button, {ButtonType, ButtonSize} from './components/Button/button'
+import Menu from './components/Menu/menu'
+import MenuItem from './components/Menu/menuItem'
 
 function App() {
   return (
     <div className="App">
+      <Menu onSelect={(index)=>{console.log(index);
+      }}>
+        <MenuItem>111</MenuItem>
+        <MenuItem>222</MenuItem>
+        <MenuItem>333</MenuItem>
+      </Menu>
       <Button >hello</Button>
       <Button btnType={ButtonType.Primary} size={ButtonSize.Large} >hello</Button>
-      <Button btnType={ButtonType.Link} href="https://www.baidu.com" >hello</Button>
-      <Button >hello</Button>
-      <Button >hello</Button>
-    
+      <Button btnType={ButtonType.Link} href="https://www.baidu.com" >Link</Button>
+      <Button btnType={ButtonType.Danger} size={ButtonSize.Large} >Danger Large</Button>
+      <Button btnType={ButtonType.Default} size={ButtonSize.Small}>Small Default</Button>
     </div>
   );
 }
